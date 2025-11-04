4c20b4a3721784b6a8c13fd71993850e10b86e15
daeunpk <daeunpark0431@naver.com>
2025-11-04 22:05:57 +0900

    pull spotify music data

--- back/src/main/java/com/wink/backend/repository/ChatSessionRepository.java
+++ back/src/main/java/com/wink/backend/repository/ChatSessionRepository.java
@@ -2,6 +2,12 @@ package com.wink.backend.repository;
 
 import com.wink.backend.entity.ChatSession;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface ChatSessionRepository extends JpaRepository<ChatSession, Long> {
+
+    Optional<ChatSession> findTopByTypeOrderByStartTimeDesc(String type);
 }
